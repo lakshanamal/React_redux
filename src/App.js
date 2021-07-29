d60015bc2fa2328248106c885c2cf17040b654fc
lakshanamal <lakshanamal100@gmail.com>
2021-07-29 11:15:39 +0530

    learn about add action

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.logged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Hello {counter}</h1>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        -
+      </button>
+      {isLogged ? <h2>Sign Out</h2> : <h2>Sign In</h2>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createStore } from "redux";
 import allReducer from "./reducers";
+import { Provider } from "react-redux";
+
 const store = createStore(
   allReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
@@ -12,7 +14,9 @@ const store = createStore(
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
